260e0a053a2d22c2ce7650a58a9c108d2f0259c7
yen5004 <119946331+yen5004@users.noreply.github.com>
2024-06-28 07:53:20 -0400

    Update tmux_loggr.sh

--- tmux_loggr.sh
+++ tmux_loggr.sh
@@ -1,1 +1,19 @@
+# Used to install tmux logging script
 
+#tmux plug in for scripting
+
+# Create .tmux file if none exisit
+cd ~
+if [ ! -d ".tmux" ]; then
+  echo ".tmux folder not found. Creating..."
+  sudo mkdir ".tmux" && sudo chmod 777 ".tmux"
+  echo ".tmux folder created successfully. - $(get_timestamp)" | tee -a $logg
+else  
+  echo ".tmux folder already exists" | tee -a $logg
+fi
+
+sudo cp $git_folder/tpm ~/.tmux/plugins/tpm
+echo 'set -g @plugin "tmux-plugins/tmux-logging' >> ~/.tmux.conf
+tmux source ~/.tmux.conf
+~/.tumux/plugins/tpm/scripts/install_plugins.sh
+echo "Tmux-logging plugin installed - $(get_timestamp)" | tee -a $logg
