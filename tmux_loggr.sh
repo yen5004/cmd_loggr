c582569c8d920aeae4eeb26a24c65c9b06ac826e
yen5004 <119946331+yen5004@users.noreply.github.com>
2024-06-28 09:17:06 -0400

    Update tmux_loggr.sh

--- tmux_loggr.sh
+++ tmux_loggr.sh
@@ -1,4 +1,4 @@
-# Used to install tmux logging script
+# Used to install tmux Plugin Manager and Script logger
 
 # Create .tmux file if none exisit
 cd ~
@@ -34,8 +34,26 @@ else
   echo ".tmux/plugins/tpm/scripts folder already exists"
 fi
 
-sudo cp $git_folder/tpm ~/.tmux/plugins/tpm
-echo 'set -g @plugin "tmux-plugins/tmux-logging' >> ~/.tmux.conf
+sudo cp $git_folder/tmux-plugins/tpm ~/.tmux/plugins/tpm
+tmux_conf=~/.tmux.conf
+plugin_lines=(
+    'set -g @plugin "tmux-plugins/tpm"'
+    'set -g @plugin "tmux-plugins/tmux-sensible"'
+    'set -g @plugin "tmux-plugins/tmux-logging"'
+    # Initialize TMUX plugin manager (keep this line at the very bottom of tmux.conf)
+    'run "~/.tmux/plugins/tpm/tpm"'
+)
+
+# Check if the file already exists
+if [ ! -f "$tmux_conf" ]; then
+    # Iterate over each plugin line and append it to the file
+    for line in "${plugin_lines[@]}"; do
+        echo "$line" >> "$tmux_conf"
+    done
+else
+    echo "File $tmux_conf already exists. Skipping append operation."
+fi
+
 tmux source ~/.tmux.conf
 ~/.tumux/plugins/tpm/scripts/install_plugins.sh
 echo "Tmux-logging plugin installed"
