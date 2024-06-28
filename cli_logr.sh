3ef487a6636f13949bf5c465c3e2707344495807
yen5004 <119946331+yen5004@users.noreply.github.com>
2024-06-28 07:37:57 -0400

    Update cli_logr.sh

--- cli_logr.sh
+++ cli_logr.sh
@@ -1,1 +1,58 @@
+#!/bin/bash
 
+###############################
+# Install command logger
+
+cd $git_folder
+sudo mkdir log && sudo chmod 777 log && cd log
+sudo touch cmd_logr_install.sh && sudo chmod 777 cmd_logr_install.sh
+cat << 'EOF' > cmd_logr_install.sh
+#Install logger script
+echo "###########Custom Script Below###########" | tee -a ~/.zshrc
+echo "Script created by Franco M." | tee -a ~/.zshrc
+echo "###########Custom Script Below###########" | tee -a ~/.bashrc
+echo "Script created by Franco M." | tee -a ~/.bashrc
+
+#Prompt username
+echo "Please enter your username"
+
+#Read user input 
+read -r name
+
+#Store username in the .zshrc
+echo "export NAME=$name" >> ~/.zshrc
+
+#Display time in terminal
+#echo 'RPROMPT="[%D{%m/%f/%Y}|%D{%L:%M}]"' >> ~/.zshrc
+echo 'RPROMPT="[%D{%d%b%Y}|%D{%L:%M}]"' >> ~/.zshrc
+
+#Sent logs to a file with time stamp
+echo 'test "$(ps -ocommand= -p $PPID | awk '\''{print $1}'\'')" == '\''script'\'' || (script -a -f $HOME/log/$(date +"%F")_shell.log)' >> ~/.zshrc
+
+#Confirm user is stored and display IP info and more
+echo "echo TED-User: '$name'" >> ~/.zshrc
+echo "ifconfig" >> ~/.zshrc
+echo "NOTE: Use EXIT to close Log Script" >> ~/.zshrc
+echo "NOTE: Use EXIT to close Log Script"
+echo 'echo $note' >> ~/.zshrc
+
+#Store username in the .bashrc
+echo "export NAME=$name" >> ~/.bashrc
+#echo 'RPROMPT="[%D{%m/%f/%Y}|%D{%L:%M}]"' >> ~/.bashrc
+echo 'RPROMPT="[%D{%d%b%Y}|%D{%L:%M}]"' >> ~/.bashrc  
+
+#Sent logs to a file with time stamp
+echo 'test "$(ps -ocommand= -p $PPID | awk '\''{print $1}'\'')" == '\''script'\'' || (script -a -f $HOME/log/$(date +"%F")_shell.log)' >> ~/.bashrc
+
+#Confirm user is stored and display IP info and more
+echo "TED-User: '$name'" >> ~/.bashrc
+echo "ifconfig" >> ~/.bashrc
+echo 'note="use exit to  close script"' >> ~/.bashrc
+echo 'echo $note' >> ~/.bashrc
+echo "Command logger install complete"
+echo "cmd_logr_install.sh finished!"
+
+EOF
+
+echo "Copied 'cmd_logr_install.sh' at: $PWD - $(get_timestamp)" | tee -a $logg
+./cmd_logr_install.sh
